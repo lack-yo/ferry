4485dcc9f50648adb5b327fc38ffbb8af1d169ac
楼锋 <loufeng@loufengdeMacBook-Pro-2.local>
2019-04-08 09:40:07 +0800

    feat: init

---
+++ src/main/java/com/ganya/FerryApplication.java
@@ -0,0 +1,13 @@
+package com.ganya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerryApplication.class, args);
+	}
+
+}
